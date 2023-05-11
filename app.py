49fc0001624c44cb540cef9c065993d8a4c0a3f1
kaiodeodato <kaiod.oficial@gmail.com>
2023-05-11 08:40:57 +0100

    headers

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 09:54:17 2021
+@author: kaiodeodato
+"""
+
 from flask import Flask
 from views import views
 
@@ -5,6 +11,6 @@ app = Flask(__name__, static_folder='static')
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run()
 
 
